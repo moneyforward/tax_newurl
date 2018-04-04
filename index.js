5f6830b7f3b1dc871f88a9390a6223368012d47b
hkdnet <satoko.itse@gmail.com>
2018-04-04 19:41:59 +0900

    Wrap

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-new Vue({
-  el: '#app',
-  data() {
-    return {
-      csv: null,
-      searchQuery: '',
-      results: [],
-      isProgress: false
-    };
-  },
-  methods: {
-    fillCSV() {
-      this.csv = $.csv.toObjects(document.getElementById("csv").text.trim());
+!function($, Vue) {
+  new Vue({
+    el: '#app',
+    data() {
+      return {
+        csv: null,
+        searchQuery: '',
+        results: [],
+        isProgress: false
+      };
     },
-    search() {
-      if (this.searchQuery === '') { return '' }
+    methods: {
+      fillCSV() {
+        this.csv = $.csv.toObjects(document.getElementById("csv").text.trim());
+      },
+      search() {
+        if (this.searchQuery === '') { return '' }
 
-      setTimeout(function() {
-        this.results = this.csv.filter(x => { 
-          return this.searchQuery.split(/\s/).every(q => {
-            return JSON.stringify(x).includes(q) 
-          })
-        });
-      }.bind(this), 0)
-    }
-  },
-  mounted() {
-    this.fillCSV();
-  },
-})
+        setTimeout(function() {
+          this.results = this.csv.filter(x => { 
+            return this.searchQuery.split(/\s/).every(q => {
+              return JSON.stringify(x).includes(q) 
+            })
+          });
+        }.bind(this), 0)
+      }
+    },
+    mounted() {
+      this.fillCSV();
+    },
+  })
+}($, Vue)
